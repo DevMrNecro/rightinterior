06de70a64ea805a6936ac1e6cdcb88f6821dad97
DevMrNecro <ojasbarik.dev@gmail.com>
2021-05-31 00:35:42 +0530

    first commit for all files

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'apps.core',
     'apps.vendor',
     'apps.product',
