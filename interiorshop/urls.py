137000696611844619475901371b3effee33f2d9
DevMrNecro <ojasbarik.dev@gmail.com>
2021-05-31 00:21:15 +0530

    first commit for all files

--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -9,8 +9,4 @@ urlpatterns = [
     path('',include('apps.core.urls')),
     path('',include('apps.product.urls')),
 
-]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
