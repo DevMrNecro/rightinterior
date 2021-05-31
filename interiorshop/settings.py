600c32bc7a8a1ea68178b463702b0aa24ad920b9
DevMrNecro <ojasbarik.dev@gmail.com>
2021-05-31 11:18:39 +0530

    first commit for all files

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -130,12 +130,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'apps/core/static'),os.path.join(BASE_DIR,'media')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'apps/core/static'),os.path.join(BASE_DIR,'apps/core/static/media')]
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 django_heroku.settings(locals())
