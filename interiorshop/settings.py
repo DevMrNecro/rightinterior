2ff959f7bcdcf32a117164ef5fda6dd8d4944ec5
DevMrNecro <ojasbarik.dev@gmail.com>
2021-05-31 13:11:08 +0530

    first commit for all files

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'apps/core/static'),os.path.join(BASE_
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles/')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 django_heroku.settings(locals())
 
