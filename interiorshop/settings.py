2d043633740bbfc15867a0c7cd4f322f2785e8b7
DevMrNecro <ojasbarik.dev@gmail.com>
2021-05-31 16:45:27 +0530

    my app first commit

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -1,8 +1,8 @@
 import os
 import django_heroku
 import dj_database_url 
-
 from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4@1dbdk2uzf+@abt1vefe1c26b-g!1&2ajy(q!40rzruoa41r8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
